abb494f7b1d2ad8a880cfadfbf511e867194e0e5
dewittsc <scott@terescode.com>
2017-11-04 21:28:25 -0400

    more tests

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 # Provision WordPress Stable
 
+WPDBNAME=manufacturingstories
+WPTABLEPREFIX=wp_crfqu5gtkb_
+WPDIR=public_html
+WPSITEURL=http://local.mfgstories.com
+
 # Nginx Logs
 mkdir -p ${VVV_PATH_TO_SITE}/log
 touch ${VVV_PATH_TO_SITE}/log/error.log
 touch ${VVV_PATH_TO_SITE}/log/access.log
 
 # Install and configure the latest stable version of WordPress
-if [[ ! -d "${VVV_PATH_TO_SITE}/public_html/index.php" ]]; then
+if [[ ! -f "${VVV_PATH_TO_SITE}/$WPDIR/index.php" ]]; then
 
   # Make a database, if we don't already have one
-  echo -e "\nCreating database 'wp_mfgstories' (if it's not already there)"
-  mysql -u root --password=root -e "DROP DATABASE manufacturingstories;"
-  mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS manufacturingstories;"
-  mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON manufacturingstories.* TO wp@localhost IDENTIFIED BY 'wp';"
+  echo -e "\nCreating database '$WPDBNAME' (if it's not already there)"
+  mysql -u root --password=root -e "DROP DATABASE $WPDBNAME;"
+  mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS $WPDBNAME;"
+  mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON $WPDBNAME.* TO wp@localhost IDENTIFIED BY 'wp';"
   echo -e "\n DB operations done.\n\n"
   
   echo "Downloading WordPress Stable, see http://wordpress.org/..."
@@ -24,28 +29,42 @@ if [[ ! -d "${VVV_PATH_TO_SITE}/public_html/index.php" ]]; then
   cp -R wordpress/* public_html
   rm -rf wordpress
   rm latest.tar.gz
-  cd ${VVV_PATH_TO_SITE}/public_html
+  cd ${VVV_PATH_TO_SITE}/$WPDIR
 
   echo "Configuring WordPress Stable..."
-  noroot wp core config --dbname=manufacturingstories --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+  noroot wp core config --dbname=$WPDBNAME --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
 define( 'WP_DEBUG', true );
-\$table_prefix = 'wp_crfqu5gtkb_';
+\$table_prefix = '$WPTABLEPREFIX';
 PHP
 
   echo "Importing production DB copy..."
-  mysql -u root --password=root wp_mfgstories < `ls ${VVV_PATH_TO_SITE}/provision/manufacturingstories*.sql`
+  mysql -u root --password=root $WPDBNAME < `ls ${VVV_PATH_TO_SITE}/provision/$WPDBNAME*.sql`
 
+  echo "Updating site URLs..."
+  noroot wp option update home "$WPSITEURL"
+  noroot wp option update siteurl "$WPSITEURL"
+  
 else
 
   echo "Updating WordPress Stable..."
-  cd ${VVV_PATH_TO_SITE}/public_html
+  cd ${VVV_PATH_TO_SITE}/$WPDIR
   noroot wp core update
 
 fi
 
 echo "Running npm install..."
 cd ${VVV_PATH_TO_SITE}
-npm install --no-bin-link
-node ./setup.js
-grunt composer:run:install
+noroot npm install
+
+if [[ ! -f "${VVV_PATH_TO_SITE}/composer.phar" ]]; then
+  echo "Downloading composer locally..."
+  noroot node ./setup.js
+fi
+
+echo "Running composer install..."
+noroot grunt composer:run:install
+
+noroot wp plugin install ${VVV_PATH_TO_SITE}/assets/plugins/blastcaster.zip --activate
+noroot wp theme install ${VVV_PATH_TO_SITE}/assets/themes/wpex-noir.zip --activate
+
 
